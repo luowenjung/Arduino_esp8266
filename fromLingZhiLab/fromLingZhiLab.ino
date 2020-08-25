49b19b538d02ad61b9c5311a82dc611841e38053
luowenjung <806198196@qq.com>
2020-08-25 19:54:04 +0800

    （less）3 second  enter smartconfig，add a feedback after connect
    以查询方式进行smartconfig，
    存在一个问题，就是触发中断时的抖动问题，会导致多次中断的出现
    少有的问题，重启后会死一会机
    增加一个循环用于等待Wi-Fi连接成功后，关闭smartconfig

--- fromLingZhiLab/fromLingZhiLab.ino
+++ fromLingZhiLab/fromLingZhiLab.ino
@@ -1,55 +1,20 @@
+void ICACHE_RAM_ATTR interrput_io4();
 #include<ESP8266WiFi.h>
-void tcp()
-{
-    
-  
-  
-  
-}
-void smartConfig()
-{
-  //if(WiFi.status() == WL_CONNECTED)
-  //{
-    //WiFi.disconnect(false);//造成第一次连接失败后，以后的连接会使用上次连接失败的密码账号。
-  //}
-  WiFi.mode(WIFI_STA);
-  Serial.println("waiting for smartconfig");
-  delay(2000);
-  WiFi.beginSmartConfig();
-  while(1)
-  {
-    Serial.print(".");
-    Serial.print("read io4 :");
-  Serial.println(digitalRead(4));
-    delay(500);
-    if(WiFi.smartConfigDone())
-    {
-      Serial.println("SmartConfig Success");
-      Serial.printf("SSID:%s\r\n", WiFi.SSID().c_str());
-      Serial.printf("PSW:%s\r\n", WiFi.psk().c_str());
-      WiFi.setAutoConnect(true);  // 设置自动连接
-      break;
-    }
-   }
-  Serial.println("");
-  Serial.println("WiFi connected");  
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-  digitalWrite(4,HIGH);
-}
+bool wifi_config=false;
+//中断里卡太久会溢出。
 
 void setup()
 {
   Serial.begin(115200);
-  pinMode(4,INPUT);
+  //pinMode(4,INPUT);
+  pinMode(4, INPUT);
   //attachInterrupt(4, smartConfig, CHANGE);//当int.0电平改变时,触发中断函数blink
-  
+  attachInterrupt(digitalPinToInterrupt(4), interrput_io4, FALLING);
   //smartConfig();  
 }
-
 void loop()
 {
-  delay(100);
+  //delay(100);
   if(WiFi.status()!= WL_CONNECTED)
   {
     Serial.println("unconnect");
@@ -61,9 +26,74 @@ void loop()
   }
   Serial.print("read io4 :");
   Serial.println(digitalRead(4));
-  if(!digitalRead(4))
+  if(wifi_config)
   {
+    detachInterrupt(4);
     smartConfig();
-    }
+    attachInterrupt(digitalPinToInterrupt(4), interrput_io4, FALLING);
+  }
   delay(500);
+}
+void interrput_io4()
+{
+    Serial.print("Enter the interrput digitalPinToInterrupt(4)");
+    Serial.println(digitalPinToInterrupt(4));
+    wifi_config=true;
+}
+void smartConfig()
+{
+  
+  //if(WiFi.status() == WL_CONNECTED)
+  //{
+    //WiFi.disconnect(false);//造成第一次连接失败后，以后的连接会使用上次连接失败的密码账号。
+  //}
+  delay(1000);
+  int count=0;
+  //持续3秒低电平
+  if(!digitalRead(4)){
+    while(count>=40)
+    {
+      delay(100);
+      if(digitalRead(4))
+        {
+          Serial.println("less than 3 second");
+          wifi_config=false;
+          return;
+        }
+        ++count;
+    }
+  }
+  Serial.println("enter smartconfig mode!");
+  WiFi.mode(WIFI_STA);
+  Serial.println("waiting for smartconfig");
+  
+  //delay(2000);
+  WiFi.beginSmartConfig();
+  while(1)
+  {
+    Serial.print(".");
+    delay(500);
+    if(WiFi.smartConfigDone())
+    {
+      Serial.println("SmartConfig Success");
+      Serial.printf("SSID:%s\r\n", WiFi.SSID().c_str());
+      Serial.printf("PSW:%s\r\n", WiFi.psk().c_str());
+      WiFi.setAutoConnect(true);  // 设置自动连接
+      break;
+    }
+   }
+  Serial.print("Wi-Fi connecting");
+  while(WiFi.status() != WL_CONNECTED)
+  {
+    delay(300);
+    Serial.printf(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  
+  Serial.println(WiFi.localIP());
+  WiFi.stopSmartConfig(); //向手机反馈连接成功
+  wifi_config=false;
+  //digitalWrite(4,HIGH);
 }
